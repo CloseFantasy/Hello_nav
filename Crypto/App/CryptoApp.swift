5e4cc100979599641f8137eab329683a288bd323
CloseFantasy <108133259+CloseFantasy@users.noreply.github.com>
2020-08-31 21:34:22 +0000

    Linux 3.10.59

--- Crypto/App/CryptoApp.swift
+++ Crypto/App/CryptoApp.swift
@@ -13,4 +13,5 @@ struct CryptoApp: App {
         WindowGroup {
             ContentView()
         }
-  
+    }
+}
---
+++ Crypto/Flows/Screens/Details/DetailsView.swift
@@ -0,0 +1,14 @@
+
+//
+//  DetailsView.swift
+//  Crypto
+//
+//  Created by Zhenya Pavlovskyi on 08.11.2022.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text("Hello")
+    }
