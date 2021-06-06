5fcb587835a2072f765c2041182afae6eb275ffa
CloseFantasy <108133259+CloseFantasy@users.noreply.github.com>
2021-06-06 18:13:17 +0000

    Rename no to nb (step 4/4): Change 'no' to 'nb' in remaining files.

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -51,4 +51,5 @@ struct MainView: View {
                                 Text("Litecoin")
                                 Spacer()
     
-                              
+                                Text("$68.06")
+ 
