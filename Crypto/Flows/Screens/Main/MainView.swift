de5772ee60c4de2c1e557f3a0667a76eb31de600
CloseFantasy <108133259+CloseFantasy@users.noreply.github.com>
2021-07-28 16:42:56 +0000

    More notes

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -57,4 +57,4 @@ struct MainView: View {
                                     .background(Color("SettingsColorForm"))
                                     .cornerRadius(10)
                                     .foregroundColor(.white)
-  
+                        
