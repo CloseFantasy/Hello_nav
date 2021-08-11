9412de3771ffc2f0c8c3f58b197ef07ba4ea2c92
CloseFantasy <108133259+CloseFantasy@users.noreply.github.com>
2021-08-11 20:22:31 +0000

    Use dist install by default

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -66,4 +66,5 @@ struct MainView: View {
                         Section {
                             HStack {
                                 Image("Ether")
-                                    .frame
+                                    .frame(width: 25)
+         
