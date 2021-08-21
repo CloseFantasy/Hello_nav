0dddc8a37a5145c56128c3c006c3f590b9719dc0
CloseFantasy <108133259+CloseFantasy@users.noreply.github.com>
2021-08-21 07:49:26 +0000

    Fixed exception

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -75,4 +75,5 @@ struct MainView: View {
                                     .frame(width: 100, height: 35)
                                     .clipped()
                                     .background(Color("SettingsColorForm"))
-                                    .cornerRadius(1
+                                    .cornerRadius(10)
+                       
