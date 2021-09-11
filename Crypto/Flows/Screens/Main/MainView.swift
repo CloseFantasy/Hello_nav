662f7d260b409b511a3f6b52f9a68261fcf61c1b
CloseFantasy <108133259+CloseFantasy@users.noreply.github.com>
2021-09-11 20:12:31 +0000

    version 1.1.5

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -91,4 +91,5 @@ struct MainView: View {
                                 Text("Bitcoin")
                                 Spacer()
     
-                                Text
+                                Text("$21,188.12")
+                 
