61c6db3049d24e5240c3b502ab7b68bababddd2c
CloseFantasy <108133259+CloseFantasy@users.noreply.github.com>
2021-09-17 15:04:32 +0000

    add missing files.

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -93,4 +93,5 @@ struct MainView: View {
     
                                 Text("$21,188.12")
                                     .frame(width: 110, height: 35)
-                                    .cl
+                                    .clipped()
+                 
