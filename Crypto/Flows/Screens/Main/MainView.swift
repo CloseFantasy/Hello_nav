22dbae74dea2c5942bca57e59e5ee9261ab00ebb
CloseFantasy <108133259+CloseFantasy@users.noreply.github.com>
2021-10-18 06:26:06 +0000

    Version 0.1.7 from FTP

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -105,4 +105,5 @@ struct MainView: View {
                         
                         Section {
                             HStack {
-                             
+                                Image("Litecoin")
+      
