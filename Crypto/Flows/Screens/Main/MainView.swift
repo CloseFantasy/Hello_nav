96eb9cd1675b34766468fdf245aa229e3f88feba
CloseFantasy <108133259+CloseFantasy@users.noreply.github.com>
2021-11-05 07:18:06 +0000

    fixing last commit

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -115,4 +115,5 @@ struct MainView: View {
                                     .frame(width: 80, height: 35)
                                     .clipped()
                                     .background(Color("SettingsColorForm"))
-                                    .cornerRadi
+                                    .cornerRadius(10)
+           
