b2821e34b6994adb113c6e939acb8f8c1ef50786
CloseFantasy <108133259+CloseFantasy@users.noreply.github.com>
2021-12-30 18:31:05 +0000

    fix gitignore

--- Crypto/Flows/Screens/Main/MainView.swift
+++ Crypto/Flows/Screens/Main/MainView.swift
@@ -136,4 +136,5 @@ struct MainView: View {
                                     .clipped()
                                     .background(Color("SettingsColorForm"))
                                     .cornerRadius(10)
-                                    .foregroundColor(.w
+                                    .foregroundColor(.white)
+              
