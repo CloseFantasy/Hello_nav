498500b036e8c29ec1073d172037667b0a733aed
CloseFantasy <108133259+CloseFantasy@users.noreply.github.com>
2022-04-15 17:09:49 +0000

    initial commit:

--- Crypto/Flows/Screens/Settings/SettingView.swift
+++ Crypto/Flows/Screens/Settings/SettingView.swift
@@ -22,4 +22,5 @@ struct SettingView: View {
                 Text("Settings")
                     .fontWeight(.heavy)
                     .font(.system(size: 25))
-                 
+                    .offset(x:145, y: 0)
+   
