67e89d9f2d7af582e9891d42f4604d95eac41a83
CloseFantasy <108133259+CloseFantasy@users.noreply.github.com>
2022-07-10 23:07:15 +0000

    ALSA: pcm : Call kill_fasync() in stream lock

--- Crypto/Flows/Screens/Settings/SettingView.swift
+++ Crypto/Flows/Screens/Settings/SettingView.swift
@@ -68,4 +68,4 @@ struct SettingView: View {
                                     .background(Color("SettingsColorForm"))
                                     .cornerRadius(10)
                                     .foregroundColor(.white)
-      
+                     
