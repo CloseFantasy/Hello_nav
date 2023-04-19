e15c173733a31ebc51af60dfb36f3e492cbcfc20
CloseFantasy <108133259+CloseFantasy@users.noreply.github.com>
2023-04-19 21:51:40 +0000

    CI fixes

--- CryptoTests/CryptoTests.swift
+++ CryptoTests/CryptoTests.swift
@@ -15,4 +15,7 @@ final class CryptoTests: XCTestCase {
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the cla
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExa
